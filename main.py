679c5ea95c4998218f294342e2cf125b17e5f55d
lone-wolve <ai21821021@utg.edu.gm>
2022-11-05 14:21:54 +0000

    changes made in the return

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from enums import Operation
 from pydantic import BaseModel
+from fastapi import HTTPException
 
 fastapi = FastAPI()
 
@@ -19,13 +20,15 @@ async def calculate(value: Calculate):
     if(value.operation_type == Operation.addition) :
         result = calculateAddition(value.x, value.y)
         
-    if(value.operation_type == Operation.subtraction) :
+    elif(value.operation_type == Operation.subtraction) :
         result = calculate_subtraction(value.x, value.y)
         
-    if(value.operation_type == Operation.multiplication) :
+    elif(value.operation_type == Operation.multiplication) :
         result = calculate_multiplication(value.x, value.y)
+    else :
+        raise HTTPException(status_code=404, detail='Operation not found')
 
-    value_dict.update ({'slackUsername': 'lonewolve', 'result': result,'operation_type':value.operation_type})
+    value_dict.update ({'slackUsername': 'lonewolve', 'operation_type':value.operation_type, 'result': result,})
 
     return value_dict
 
