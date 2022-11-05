315ef63df02eb04371a24801f9152c6aa1e2e990
lone-wolve <ai21821021@utg.edu.gm>
2022-11-05 14:36:36 +0000

    changes made in the return

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def calculate(value: Calculate):
     else :
         raise HTTPException(status_code=404, detail='Operation not found')
 
-    value_dict ({'slackUsername': 'lonewolve', 'operation_type':value.operation_type, 'result': result,})
+    value_dict ({'slackUsername': 'lonewolve', 'operation_type':Operation, 'result': result,})
 
     return value_dict
 
